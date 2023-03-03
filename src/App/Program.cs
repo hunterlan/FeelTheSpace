b06b89681a981f0ca7de534475556ae041158587
hunterlan <konstantin.sharykin@outlook.com>
2023-03-03 17:44:53 +0100

    Write function count space between walls

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -1,14 +1,20 @@
-﻿var arr = new int[] {0, 7, 2, 9, 0, 4, 6, 7};
+﻿int CountSpaceCanBeFilled(int indexFirstWall, int indexSecondWall, int[] wallsArray)
+{
+    int sum = 0, lowestValueWall = indexFirstWall.CompareTo(indexSecondWall) >= 0 ? wallsArray[indexSecondWall] : wallsArray[indexFirstWall];
+
+    for (int i = indexFirstWall + 1; i < indexSecondWall; i++)
+    {
+        sum += lowestValueWall - wallsArray[i];
+    }
+
+    return sum;
+}
+
+var arr = new int[] {0, 7, 2, 9, 0, 4, 6, 7};
 
 int indexFirstHighestWall = 0, indexSecondHighestWall = 0, valueFirstHighestWall = arr[0], valueSecondHighestWall = -1, sum = 0;
+bool searchingSecondWall = false;
 
-for (int i = 1; i < arr.Length; i++)
-{
-    if (arr[i] > valueFirstHighestWall)
-    {
-        indexFirstHighestWall = i;
-        valueFirstHighestWall = arr[i];
-    }
-}
+
 
 Console.WriteLine($"Result is {sum}");
