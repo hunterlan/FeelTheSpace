94c05dfa28018e5a38c24459b762aa855e05ebfb
hunterlan <konstantin.sharykin@outlook.com>
2023-03-03 18:02:49 +0100

    Add helpful function

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -10,11 +10,37 @@
     return sum;
 }
 
-var arr = new int[] {0, 7, 2, 9, 0, 4, 6, 7};
+int[] GetMaxValueIndexes(int[] array)
+{
+    int maxValue = array.Max();
+    List<int> indexes = new List<int>();
 
-int indexFirstHighestWall = 0, indexSecondHighestWall = 0, valueFirstHighestWall = arr[0], valueSecondHighestWall = -1, sum = 0;
-bool searchingSecondWall = false;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == maxValue)
+        {
+            indexes.Add(i);
+        }
+    }
 
+    return indexes.ToArray();
+}
 
+var arr = new[] {0, 7, 2, 9, 0, 4, 6, 7};
+int sum = 0;
+
+/*int indexFirstHighestWall = 0, indexSecondHighestWall = 0, valueFirstHighestWall = arr[0], valueSecondHighestWall = -1;
+bool searchingSecondWall = false;*/
+
+var indexesMaxValue = GetMaxValueIndexes(arr);
+
+if (indexesMaxValue.Length == 2 && indexesMaxValue.ElementAt(1) == 0 && indexesMaxValue.ElementAt(1) == arr.Length - 1)
+{
+    sum = CountSpaceCanBeFilled(0, arr.Length - 1, arr);
+}
+else if (indexesMaxValue.Length != arr.Length)
+{
+    
+}
 
 Console.WriteLine($"Result is {sum}");
