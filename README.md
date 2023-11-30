11cd67e9c3224cf136f502f5e5821a68387107b4
hunterlan <konstantin.sharykin@outlook.com>
2023-11-30 17:23:37 +0100

    Task is solved

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 You have a random array which contains a height of walls.
 The aim is to count how many empty space can be filled.
-Imagine, that empty spaced will be filled by grains and it means that if one of the wall's side will be lower then another, it will be overflowed.
+Imagine, that empty space will be filled by grains and it means that if one of the wall's side will be lower then another, it will be overflowed.
 It's not necessary, that wall can be on the edges, count of them can be any and they can be side by side.
-
+![Example](./assets/example_1.png)
 **Example of array:** [0, 7, 2, 9, 0, 4, 6, 7]
--- src/App/Program.cs
+++ src/App/Program.cs
@@ -1,46 +1,71 @@
-﻿int CountSpaceCanBeFilled(int indexFirstWall, int indexSecondWall, int[] wallsArray)
+﻿IEnumerable<Wall> CreateWalls(IReadOnlyList<int> arr)
 {
-    int sum = 0, lowestValueWall = indexFirstWall.CompareTo(indexSecondWall) >= 0 ? wallsArray[indexSecondWall] : wallsArray[indexFirstWall];
+    List<Wall> walls = new ();
 
-    for (int i = indexFirstWall + 1; i < indexSecondWall; i++)
+    var isWallExists = false;
+    var wallStartIndex = 0;
+
+    for (var i = 0; i < arr.Count; i++)
     {
-        sum += lowestValueWall - wallsArray[i];
+        if (arr[i] == 0 && isWallExists is false) { }
+        else if (arr[i] != 0 && isWallExists is false)
+        {
+            isWallExists = true;
+            wallStartIndex = i;
+        }
+        else
+        {
+
+            if (i != arr.Count - 1 && arr[i] > arr[i + 1] && arr[i] >= arr[wallStartIndex])
+            {
+                walls.Add(new Wall {StartIndex = wallStartIndex, EndIndex = i});
+                wallStartIndex = i;
+            }
+            else if (i == arr.Count - 1)
+            {
+                var j = i;
+                for (; j > wallStartIndex; j--)
+                {
+                    if (arr[j] >= arr[i]) continue;
+                    walls.Add(new Wall { StartIndex = wallStartIndex, EndIndex = i });
+                    break;
+                }
+            }
+        }
+    }
+
+    return walls;
+}
+
+int SumEmptySpace(IEnumerable<Wall> walls, IReadOnlyList<int> arr)
+{
+    var sum = 0;
+
+    foreach (var wall in walls)
+    {
+        var firstWallHeights = arr[wall.StartIndex];
+        var secondWallHeight = arr[wall.EndIndex];
+        var minWallHeight = firstWallHeights > secondWallHeight ? secondWallHeight : firstWallHeights;
+
+        for (var i = wall.StartIndex + 1; i <= wall.EndIndex-1; i++)
+        {
+            sum += minWallHeight - arr[i];
+        }
     }
 
     return sum;
 }
 
-int[] GetMaxValueIndexes(int[] array)
-{
-    int maxValue = array.Max();
-    List<int> indexes = new List<int>();
-
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] == maxValue)
-        {
-            indexes.Add(i);
-        }
-    }
-
-    return indexes.ToArray();
-}
-
 var arr = new[] {0, 7, 2, 9, 0, 4, 6, 7};
-int sum = 0;
 
-/*int indexFirstHighestWall = 0, indexSecondHighestWall = 0, valueFirstHighestWall = arr[0], valueSecondHighestWall = -1;
-bool searchingSecondWall = false;*/
-
-var indexesMaxValue = GetMaxValueIndexes(arr);
-
-if (indexesMaxValue.Length == 2 && indexesMaxValue.ElementAt(1) == 0 && indexesMaxValue.ElementAt(1) == arr.Length - 1)
-{
-    sum = CountSpaceCanBeFilled(0, arr.Length - 1, arr);
-}
-else if (indexesMaxValue.Length != arr.Length)
-{
-    
-}
+var walls = CreateWalls(arr);
+var sum = SumEmptySpace(walls, arr);
 
 Console.WriteLine($"Result is {sum}");
+
+internal class Wall
+{
+    public int StartIndex { get; set; }
+
+    public int EndIndex { get; set; }
+}
